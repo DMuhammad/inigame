c2260d5b8f0389aca822833ac1f6b5ed50bfe628
DMuhammad <notfnd123@gmail.com>
2023-10-11 14:00:35 +0700

    Enemy Logic and Navmesh

--- Assets/Scripts/EnemyLogic.cs
+++ Assets/Scripts/EnemyLogic.cs
@@ -1,22 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyLogic : MonoBehaviour
 {
     public float hitPoints = 100f;
+    public float turnSpeed = 15f;
+    public Transform target;
+    public float ChaseRange;
+    private NavMeshAgent agent;
+    private float DistancetoTarget;
+    private Animator anim;
 
     // Start is called before the first frame update
-    //void Start()
-    //{
-        
-    //}
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        anim = this.GetComponentInChildren<Animator>();
+    }
 
     // Update is called once per frame
-    //void Update()
-    //{
-        
-    //}
+    private void Update()
+    {
+        DistancetoTarget = Vector3.Distance(target.position, transform.position);
+
+        if (DistancetoTarget <= ChaseRange)
+        {
+            if (DistancetoTarget > agent.stoppingDistance)
+            {
+                ChaseTarget();
+            } else if (DistancetoTarget <= agent.stoppingDistance)
+            {
+                Attack();
+            }
+        }
+    }
+
+    private void FaceTarget(Vector3 destination)
+    {
+        Vector3 direction = (destination - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+    }
+
+    public void Attack()
+    {
+        Debug.Log("attack");
+        anim.SetBool("Run", false);
+        anim.SetBool("Attack", true);
+    }
+
+    public void ChaseTarget()
+    {
+        agent.SetDestination(target.position);
+        anim.SetBool("Run", true);
+        anim.SetBool("Attack", false);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, ChaseRange);
+    }
 
     public void TakeDamage(float damage)
     {
